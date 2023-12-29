ceb31cbf8f5772de5757b6b383c94842abb668fb
StanislawMaciejewski <stas.maciejewski99@hotmail.com>
2023-12-29 10:30:32 +0100

    lab08 example has been cleaned

--- lab08example/run/INCA_libs/run.lnx86.21.03.d/files.ts
+++
@@ -1,8 +0,0 @@
-1701953775 /home/student/smaciejewski/cds.lib
-1702144095 /home/student/smaciejewski/VDIC_2023/lab08example/tb/tinyalu_pkg.sv
-1702144095 /home/student/smaciejewski/VDIC_2023/lab08example/tb/tinyalu_tpgen_module.sv
-1702144095 /home/student/smaciejewski/VDIC_2023/lab08example/tb/tinyalu_bfm.sv
-1696514400 /home/student/smaciejewski/VDIC_2023/common/tinyalu/single_cycle.sv
-1696515437 /home/student/smaciejewski/VDIC_2023/common/tinyalu/three_cycle.sv
-1696516184 /home/student/smaciejewski/VDIC_2023/common/tinyalu/tinyalu.sv
-1702144095 /home/student/smaciejewski/VDIC_2023/lab08example/tb/top.sv
